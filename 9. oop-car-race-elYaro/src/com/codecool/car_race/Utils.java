a2bc40129ad5cf919dc8bb5fc79f51f6e1ff82c0
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-11-19 21:54:37 +0100

    method creating random names for cars consisted of 2 words

--- 9. oop-car-race-elYaro/src/com/codecool/car_race/Utils.java
+++ 9. oop-car-race-elYaro/src/com/codecool/car_race/Utils.java
@@ -3,15 +3,17 @@ package com.codecool.car_race;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Utils {
 
     /**
-     * @author: elYaro
-     * @returns: String
      * this method reads the txt file with 10 given names for the Cars.
-     * Then it takes the next name for a new instance of the Car class
+     * then adds each name to the ArrayList
+     * @author: elYaro
+     * @input: txt file
+     * @return: ArrayList of Strings with names
      */
     ArrayList<String> getNamesForCar() {
         File file = new File("/Users/Yaro/repository/Java/Java SI Assignments/9. oop-car-race-elYaro/carsNames.txt");
@@ -28,5 +30,24 @@ public class Utils {
         }
         return listOfCarNames;
     }
-}
+
+
+    /**
+     * this method generates 2 numbers from 0 till 10.
+     * then it takes two names (with randomly picked index) from ArrayList and concatenates them into one String
+     *@author: elYaro
+     *@return: String (two words in one string)
+     */
+    void createNameForCar() {
+        String carName = "";
+        Random random = new Random();
+        int numberOfNamesInTxtFile = 10;
+        for (int i = 0; i <2; i++) {
+            int randomNumber = random.nextInt(numberOfNamesInTxtFile);
+            carName += getNamesForCar().get(randomNumber) + " ";
+        }
+        System.out.println(carName);
+        }
+    }
+
 
